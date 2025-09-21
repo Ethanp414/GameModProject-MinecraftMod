a8d5c6035fac779b01253d2cce7c05062544ef60
Ethan Peterson <ethanpeterson414@gmail.com>
2025-09-21 15:22:22 -0500

    Initial commit

---
+++ src/main/java/dibs/bossfight/DePaulDibsBossFightClient.java
@@ -0,0 +1,31 @@
+package dibs.bossfight;
+
+import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+// This class will not load on dedicated servers. Accessing client side code from here is safe.
+@Mod(value = DePaulDibsBossFight.MODID, dist = Dist.CLIENT)
+// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+@EventBusSubscriber(modid = DePaulDibsBossFight.MODID, value = Dist.CLIENT)
+public class DePaulDibsBossFightClient {
+    public DePaulDibsBossFightClient(ModContainer container) {
+        // Allows NeoForge to create a config screen for this mod's configs.
+        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
+        // Do not forget to add translations for your config options to the en_us.json file.
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event) {
+        // Some client setup code
+        DePaulDibsBossFight.LOGGER.info("HELLO FROM CLIENT SETUP");
+        DePaulDibsBossFight.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+    }
+}
