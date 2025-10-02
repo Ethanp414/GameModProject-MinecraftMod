e573c411b50c9fb7282ac17246fe9f2fd15dbc78
grodd-bondrow <122652735+grodd-bondrow@users.noreply.github.com>
2025-10-02 13:20:15 -0500

    Update ModCreativeModeTabs.java

--- src/main/java/dibs/bossfight/item/ModCreativeModeTabs.java
+++ src/main/java/dibs/bossfight/item/ModCreativeModeTabs.java
@@ -5,10 +5,28 @@ import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.CreativeModeTab;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+import net.minecraft.world.item.ShovelItem;
+
 public class ModCreativeModeTabs {
 
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB =
             DeferredRegister.create(BuiltInRegistries.CREATIVE_MODE_TAB, DePaulDibsBossFight.MODID);
+            
+            
+    public static final Supplier<CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TAB.register("example", () -> CreativeModeTab.builder()
+    //Set the title of the tab. Don't forget to add a translation!
+    .title(Component.translatable("itemGroup." + MOD_ID + ".example"))
+    //Set the icon of the tab.
+    .icon(() -> new ItemStack(MyItemsClass.EXAMPLE_ITEM.get()))
+    //Add your items to the tab.
+    .displayItems((params, output) -> {
+        output.accept(MyItemsClass.MY_ITEM.get());
+        // Accepts an ItemLike. This assumes that MY_BLOCK has a corresponding item.
+        output.accept(MyBlocksClass.MY_BLOCK.get());
+    })
+    .build()
+);
+
 
 
     /* 
