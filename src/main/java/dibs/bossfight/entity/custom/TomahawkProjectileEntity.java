3b1e19feb8ae58238ed7b24984c1d50f6b9bca60
grodd-bondrow <122652735+grodd-bondrow@users.noreply.github.com>
2025-10-14 13:20:57 -0500

    Update TomahawkProjectileEntity.java

--- src/main/java/dibs/bossfight/entity/custom/TomahawkProjectileEntity.java
+++ src/main/java/dibs/bossfight/entity/custom/TomahawkProjectileEntity.java
@@ -13,7 +13,7 @@ import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.Vec2;
 
-public class TomahawkProjectileEntity extends AbstractArrow implements net.minecraft.client.model.EntityRenderState {
+public class TomahawkProjectileEntity extends AbstractArrow {
     private float rotation;
     public Vec2 groundedOffset;
 
