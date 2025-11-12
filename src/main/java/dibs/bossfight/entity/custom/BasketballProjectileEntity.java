ff7de78e9f1e4974521b16e7a8fba105ef9a5f3b
grodd-bondrow <122652735+grodd-bondrow@users.noreply.github.com>
2025-11-11 20:02:13 -0600

    Update BasketballProjectileEntity.java

--- src/main/java/dibs/bossfight/entity/custom/BasketballProjectileEntity.java
+++ src/main/java/dibs/bossfight/entity/custom/BasketballProjectileEntity.java
@@ -70,13 +70,16 @@ public class BasketballProjectileEntity extends ThrowableItemProjectile {
       this.playSound(SoundEvents.BUBBLE_POP);
 
       float randomFloat = (float)Math.random();
-      if (randomFloat < 0.65f) {
+      if (randomFloat < 0.7f) {
        this.explosionPower = 1;
-      } else if (randomFloat < 0.95f) {
+      } else if (randomFloat < 0.99f) {
        this.explosionPower = 2;
-      } else {
+      } else if (randomFloat < 0.999f){
        this.explosionPower = 8;
+      } else {
+       this.explosionPower = 16;
       }
+
       this.level().explode(this, this.getX(), this.getY(), this.getZ(), (float)this.explosionPower, true, Level.ExplosionInteraction.MOB);
 
       if (!this.level().isClientSide()) {
